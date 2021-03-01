ba8a41ef44c7d91cd62ca635f5f2946c8f3f0c64
Beff-Jezos <67267328+Beff-Jezos@users.noreply.github.com>
2021-03-01 15:41:15 +0100

    Update main.go
    bugfix

--- main.go
+++ main.go
@@ -169,9 +169,10 @@ func form(w http.ResponseWriter, r *http.Request){
 	PLad := r.FormValue("Laden")
 
     
+	formTemplate, _ := template.ParseFiles("static/formular.html")
+	formTemplate.Execute(w, nil)
 
-
-	tpl.ExecuteTemplate(w, "formular.html")
+	//tpl.ExecuteTemplate(w, "formular.html")
 }
 
 
