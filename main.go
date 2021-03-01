c884d000750aa0026221bc0d5eac634f9832f7c7
Beff-Jezos <67267328+Beff-Jezos@users.noreply.github.com>
2021-03-01 15:49:25 +0100

    Update main.go

--- main.go
+++ main.go
@@ -202,7 +202,7 @@ func main() {
 
 	http.Handle("/", m)
 	http.Handle("/merkliste", http.HandlerFunc(ListHandler))
-	http.HandleFunc("/formular", http.HandlerFunc(ListHandler))
+	http.Handle("/formular", http.HandlerFunc(ListHandler))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	
 
