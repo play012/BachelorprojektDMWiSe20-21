a40caae500250751a8e44038a797586fb1af3322
unknown <s.friesen1@protonmail.com>
2021-03-07 23:36:53 +0100

    added navbar styling

--- static/index.js
+++ static/index.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    document.getElementById("navList").style.top = document.getElementById("header").offsetHeight;
     if (localStorage.length != 0) {
         loadButtonOnHomepage();
     }
@@ -6,4 +7,16 @@ window.onload = function() {
 
 function loadButtonOnHomepage() {
     document.body.innerHTML += '<a href="/merkliste" class="merkliste"></a>';
+}
+
+function changeUlDisplay() {
+    var navList = document.getElementById("navList");
+    var navIcon = document.getElementById("navIcon");
+    if (navList.style.display == "none") {
+        navList.style.display = "block";
+        navIcon.innerHTML = "X";
+    } else {
+        navList.style.display = "none";
+        navIcon.innerHTML = "&#x2630;";
+    }
 }
