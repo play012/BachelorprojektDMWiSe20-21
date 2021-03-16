f6b0788c5808f2bf40e628f9c1b1ba2390abeb86
unknown <s.friesen1@protonmail.com>
2021-03-16 03:40:37 +0100

    fixed footer and addToBasket animation

--- static/addToBasket.js
+++ static/addToBasket.js
@@ -1,21 +1,22 @@
-var eventHandler = document.getElementsByClassName("addItemBtn");
 var iBox = document.getElementById('infoBox');
-
 
 var objects = [iBox,iBox,iBox,iBox,iBox,iBox,iBox,iBox,iBox];
 var counters = [0,0,0,0,0,0,0,0,0,0,0,,0,0];
 var transformObj;
 var elem;
+var elemArr = [];
 
 var indexClick = 0;
 var o = 1;
 
 
-eventHandler.onclick = function() {
+function animateAddToBasket(itemID) {
 
     //infoBox clonen, neu benennen
     elem = iBox.cloneNode(true);
     elem.id = "infoBox" + indexClick.toString();
+    elemArr.push(itemID);
+    
     iBox.appendChild(elem); // An Ibox wird elem angehängt
     
     //img erstellen und anhängen
@@ -23,13 +24,12 @@ eventHandler.onclick = function() {
     img.id = "img";
 
     //img einfügen richtig verlinken bitte :) ----------------------------------------------
-    img.src = "/static/addBasket.png";
+    img.src = "/static/plus1.webp";
 
     elem.appendChild(img);  // An elem wird img gehängt
     //img stylen
     img.style.width = "100px";
     img.style.height = "100px";
-    img.style.marginTop = "50px";
     
     //elem in Array setzen -> keine überschneidungen
     objects[indexClick] = elem;
@@ -43,7 +43,7 @@ eventHandler.onclick = function() {
     console.log("IndexClick: %d", indexClick);
     
     //Style Object
-    obj.style.position = "relative";
+    obj.style.position = "fixed";
     obj.style.width = "100px";
     obj.style.height = "100px";
     obj.style.textAlign = "center";
@@ -58,9 +58,6 @@ eventHandler.onclick = function() {
     // -> Moving Object (through moveObj function)
     var myInterval = setInterval(moveObj, 12);  
 
-
-
-
     function moveObj(){
         //up to stepCount -> move Object
         if (stepCount <= 140) {
@@ -69,11 +66,11 @@ eventHandler.onclick = function() {
             console.log("Object %d, TransformValue: %d",indexClick ,transformObj)
             // MovementLength/HoldLength
             if (stepCount < 50) {
-                obj.style.transform = "translateX(-"+transformObj.toString()+"px)";
+                obj.style.transform = "translateY(-"+transformObj.toString()+"px)";
             }
             //step Size (=animation speed)
             stepCount+=4;
-
+    
             // Use last steps for OpacityFade
         } else {
             obj.style.opacity = o.toString();
@@ -91,5 +88,7 @@ eventHandler.onclick = function() {
     
     }
 
+    if (elemArr.includes(itemID)) {
+        document.getElementById("addItemBtn" + itemID).onclick = "";
+    }
 }
-
